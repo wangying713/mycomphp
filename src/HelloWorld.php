ae1f5b01f57f15620f0d5fc950b13beaf6064bd7
ying <ying@yingdeMacBook-Pro.local>
2020-07-03 16:40:19 +0800

    x

---
+++ src/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scort
+ * Date: 18-3-11
+ * Time: 上午11:16
+ */
+namespace HelloWorld;
+
+class HelloWorld
+{
+    public function __construct()
+    {   
+        echo "Hello World!";
+    }
+}
